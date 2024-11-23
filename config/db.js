31c7db94caac043d766f410e211d1703391ffa44
ValentinVitor <vitorvalentin14@hotmail.com>
2024-11-23 00:50:12 -0300

    feature: banco de dados aws rds

--- config/db.js
+++ config/db.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 // Configuração do banco de dados
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "gestor_android"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10, // Limite de conexões simultâneas
+  queueLimit: 0,
 
 });
 
